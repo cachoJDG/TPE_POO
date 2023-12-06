33ac5dc67006f69dc55f82aacef2da6b0a4a7134
juangago <jgago@itba.edu.ar>
2023-12-06 13:40:55 -0300

    fix Circle extends Ellipse

--- src/backend/model/Circle.java
+++ src/backend/model/Circle.java
@@ -1,26 +1,24 @@
 package backend.model;
 
-public class Circle implements Figure {
+public class Circle extends Ellipse implements Figure  {
 
-    protected final Point centerPoint;
-    protected final double radius;
+//    protected final Point centerPoint;
+//    protected final double radius;
 
     public Circle(Point centerPoint, double radius) {
-        this.centerPoint = centerPoint;
-        this.radius = radius;
+        super(centerPoint, radius, radius);
+//        this.centerPoint = centerPoint;
+//        this.radius = radius;
     }
 
     @Override
     public String toString() {
-        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, radius);
+        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, sMinorAxis);
     }
 
-    public Point getCenterPoint() {
-        return centerPoint;
-    }
 
     public double getRadius() {
-        return radius;
+        return sMinorAxis;
     }
 
 }

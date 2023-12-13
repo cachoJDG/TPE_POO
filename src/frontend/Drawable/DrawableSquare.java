dab7b3f9ff54dc2c55255a7e29326bc32e7dcc77
juangago <jgago@itba.edu.ar>
2023-12-13 19:07:06 -0300

    DrawableSquare extendes DrawableRectangle

--- src/frontend/Drawable/DrawableSquare.java
+++ src/frontend/Drawable/DrawableSquare.java
@@ -5,27 +5,27 @@ import backend.model.Square;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class DrawableSquare extends Square implements Drawable{
+public class DrawableSquare extends DrawableRectangle implements Drawable{
 
     private final Color color;
 
     public DrawableSquare(Point topLeft, double size, Color color) {
+        super(topLeft, new Point(topLeft.getX() + size, topLeft.getY() + size), color);
 
-        super(topLeft,size);
         this.color = color;
     }
 
-    @Override
-    public void draw(GraphicsContext gc) {
-        gc.setFill(color);
-        if(isSelected())
-        {
-            gc.setStroke(Color.RED);
-        }
-        gc.fillRect(getTopLeft().getX(), getTopLeft().getY(),
-                Math.abs(getTopLeft().getX() - getBottomRight().getX()), Math.abs(getTopLeft().getY() - getBottomRight().getY()));
-        gc.strokeRect(getTopLeft().getX(), getTopLeft().getY(),
-                Math.abs(getTopLeft().getX() - getBottomRight().getX()), Math.abs(getTopLeft().getY() - getBottomRight().getY()));
-
-    }
+//    @Override
+//    public void draw(GraphicsContext gc) {
+//        gc.setFill(color);
+//        if(isSelected())
+//        {
+//            gc.setStroke(Color.RED);
+//        }
+//        gc.fillRect(getTopLeft().getX(), getTopLeft().getY(),
+//                Math.abs(getTopLeft().getX() - getBottomRight().getX()), Math.abs(getTopLeft().getY() - getBottomRight().getY()));
+//        gc.strokeRect(getTopLeft().getX(), getTopLeft().getY(),
+//                Math.abs(getTopLeft().getX() - getBottomRight().getX()), Math.abs(getTopLeft().getY() - getBottomRight().getY()));
+//
+//    }
 }

e0f824d6d9a58038b65a12e2941bf2933646a06a
Juan Bautista Albertoni Salini <juanbaalbertoni@gmail.com>
2023-12-14 13:14:31 -0300

    Rectangulo y cuadraado Shadow and gradient

--- src/frontend/Drawable/DrawableRectangle.java
+++ src/frontend/Drawable/DrawableRectangle.java
@@ -5,6 +5,9 @@ import backend.model.Rectangle;
 import javafx.scene.canvas.GraphicsContext;
 
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 
 public class DrawableRectangle extends Rectangle implements Drawable {
 
@@ -33,6 +36,21 @@ public class DrawableRectangle extends Rectangle implements Drawable {
 
     @Override
     public void shadow() {
+        gc.setFill(Color.GRAY);
+        gc.fillRect(getTopLeft().getX() + 10.0,
+                getTopLeft().getY() + 10.0,
+                Math.abs(getTopLeft().getX() - getBottomRight().getX()),
+                Math.abs(getTopLeft().getY() - getBottomRight().getY()));
+        gc.setFill(color);
 
     }
+    @Override
+    public void gradient() {
+        LinearGradient linearGradient = new LinearGradient(0, 0, 1, 0, true,
+                CycleMethod.NO_CYCLE,
+                new Stop(0, color),
+                new Stop(1, color.invert()));
+        gc.setFill(linearGradient);
+    }
+
 }

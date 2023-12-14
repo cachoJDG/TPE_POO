f108981376b669b4436873d6100f5c4b43cc3c83
juangago <jgago@itba.edu.ar>
2023-12-14 13:26:13 -0300

    DrawableEllipse

--- src/frontend/Drawable/DrawableEllipse.java
+++ src/frontend/Drawable/DrawableEllipse.java
@@ -5,6 +5,9 @@ import backend.model.Point;
 import javafx.scene.canvas.GraphicsContext;
 
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 
 public class DrawableEllipse extends Ellipse implements Drawable {
 
@@ -21,7 +24,6 @@ public class DrawableEllipse extends Ellipse implements Drawable {
     @Override
     public void draw(GraphicsContext gc) {
 
-        gc.setFill(color);
         if(isSelected())
         {
             gc.setStroke(Color.RED);
@@ -29,8 +31,29 @@ public class DrawableEllipse extends Ellipse implements Drawable {
         gc.strokeOval(getCenterPoint().getX() - (getsMayorAxis() / 2), getCenterPoint().getY() - (getsMinorAxis() / 2), getsMayorAxis(),getsMinorAxis());
         gc.fillOval(getCenterPoint().getX() - (getsMayorAxis() / 2), getCenterPoint().getY() - (getsMinorAxis() / 2), getsMayorAxis(), getsMinorAxis());
 
+
     }
 
+    public void shadow()
+    {
+        draw.setFill(Color.GRAY);
+        draw.fillOval(getCenterPoint().getX() - (getsMayorAxis() / 2) + 10, getCenterPoint().getY() - (getsMinorAxis() / 2) +10, getsMayorAxis(), getsMinorAxis());
+    }
+
+    public void belved()
+    {
+
+
+    }
+
+    public void gradient()
+    {
+        RadialGradient radialGradient = new RadialGradient(0.5, 0, 0.5, 0.5, 0.5, true,
+                CycleMethod.NO_CYCLE,
+                new Stop(0, color),
+                new Stop(1, color.invert()));
+        draw.setFill(radialGradient);
+    }
 
 
 
